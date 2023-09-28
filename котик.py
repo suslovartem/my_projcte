4e662fc28abab04622adef96e31e388b2fe6541f
Artem Suslov <119869442+suslovartem@users.noreply.github.com>
2023-09-28 22:29:10 +0500

    Add files via upload
    котик

---
+++ котик.py
@@ -0,0 +1,28 @@
+import play
+
+cat = play.new_text("=^.^=", font_size=70)
+
+
+@play.repeat_forever
+async def move_cat():
+    cat.x = play.random_number(-200, 200)
+    cat.y = play.random_number(-200, 200)
+    cat.color = play.random_color()
+
+    cat.show()
+
+    await play.timer(seconds=0.4)
+
+    cat.hide()
+
+    await play.timer(seconds=0.4)
+
+
+@cat.when_clicked
+def win_function():
+    cat.show()
+
+    cat.words = "You win!"
+
+
+play.start_program()
